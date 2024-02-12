1bcb7c2ad0dc5ff051123db33a8b18af0986d82c
YarikRogatkin <iaroslavrogatkin2000@gmail.com>
2024-02-12 20:45:54 +0400

    update

--- migrations/first.sql
+++ migrations/first.sql
@@ -1,8 +1,56 @@
-    create table customers(
-      ID integer generated always as identity ,
-           FirstName   CHARACTER VARYING(30),
-           LastName CHARACTER VARYING(30),
-           Email CHARACTER VARYING(30),
-           Age INTEGER
+create table industrial_area(
+    id bigint generated always as identity primary key unique ,
+    name varchar,
+    x_length real,
+    y_length real,
+    x real,
+    y real
+);
+create table transform
+(
+    id      bigint generated always as identity primary key unique,
+    nominal int,
+    cost    int,
+    model   varchar,
+    uvn real,
+    unn real,
+    ukz real,
+    ikz real
+);
+create table building(
+                         id                 bigint generated always as identity primary key unique,
+                         square             bigint,
+                         power              bigint,
+                         name               varchar,
+                         description        text,
+                         industrial_area_id bigint,
+    foreign key (industrial_area_id) references industrial_area(id)
+);
+create table building_transform(
+                                   id bigint generated always as identity primary key unique,
+                                   transform_id bigint,
+                                   building_id bigint,
+                                   x real,
+                                   y real,
+                                   foreign key (transform_id) references transform(id),
+                                       foreign key (building_id) references building(id)
+);
+create table input_point(
+    id bigint generated always as identity primary key unique,
+    building_id bigint,
+    building_transform_id bigint,
+    x real,
+    y real,
+    foreign key (building_id) references building(id),
+    foreign key (building_transform_id) references building_transform(id)
+);
+create table coords(
+                       id bigint generated always as identity primary key unique ,
+                       building_id bigint,
+                       x real,
+                       y real,
+                       foreign key (building_id) references building(id)
+);
 
-    );
+
+
--- migrations/second.sql
+++ migrations/second.sql
@@ -1,1 +1,2 @@
-insert into customers(firstname, lastname, email, age) values ('имя','фамилия','email', 15);
+insert into industrial_area(name, x_lenght, y_lenght, x,y) values ('Ремонтно-механический завод',1800, 1284, 1800, 642);
+insert into transform( id, nominal, cost, model, uvn, unn, ukz, ikz) values ( 23, 2500000, 3676000, 'ТСЗ-2500/10/0,4', 10, 0.4,6,0);
